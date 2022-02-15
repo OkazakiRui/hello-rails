27ec9ad1c8fab834786180153eb9ca6d3203c0da
RuiOkazaki <okazaki200226@gmail.com>
2022-02-16 00:27:26 +0900

    add: 新規作成のviewを作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,24 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    # 成功したらtrueが返却
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # リクエストで変更を許可するカラム名を宣言
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
